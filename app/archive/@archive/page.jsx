b93293edd63dd9d7bd7f1a4c24588349164a1087
Eliezer Nsengi <iamnsengi@icloud.com>
2022-03-13 18:44:57 +0200

    ft: add navigation on archived news

--- app/archive/@archive/page.jsx
+++ app/archive/@archive/page.jsx
@@ -1,22 +1,5 @@
-import { getAvailableNewsYears } from "@/lib/news";
-import Link from "next/link";
-
 const ArchivePage = () => {
-  const links = getAvailableNewsYears();
-
-  return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link} </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+  return <></>;
 };
 
 export default ArchivePage;
