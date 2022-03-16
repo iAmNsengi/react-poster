5fb0c413056a8e5d9294723ed319f33d887760e1
Eliezer Nsengi <iamnsengi@icloud.com>
2022-03-16 19:13:29 +0200

    ft: render posts from selected years

--- app/archive/@archive/[[...filter]]/page.jsx
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -5,20 +5,35 @@ import React from "react";
 
 const FilteredNewsPage = ({ params }) => {
   const filter = params.filter;
-  console.log(filter);
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+  let news;
+
+  if (selectedYear) {
+    news = getNewsForYear(selectedYear);
+  }
+  let newsContent = <p>No news found for the selected period!</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
   const links = getAvailableNewsYears();
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link} </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => (
+              <li key={link}>
+                <Link href={`/archive/${link}`}>{link} </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 };
 
