5460ab50e22ef44d4bd8b80cdfb476075fe288aa
Eliezer Nsengi <iamnsengi@icloud.com>
2022-03-17 19:37:13 +0200

    ft: render filter for months for posts

--- app/archive/@archive/[[...filter]]/page.jsx
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -1,5 +1,10 @@
 import NewsList from "@/components/NewsList";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 import React from "react";
 
@@ -8,27 +13,40 @@ const FilteredNewsPage = ({ params }) => {
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
   let news;
+  let links = getAvailableNewsYears();
 
-  if (selectedYear) {
+  if (selectedYear && !selectedMonth) {
     news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
   }
+
   let newsContent = <p>No news found for the selected period!</p>;
 
+  if (selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = getAvailableNewsMonths(selectedYear);
+  }
   if (news && news.length > 0) {
     newsContent = <NewsList news={news} />;
   }
 
-  const links = getAvailableNewsYears();
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map((link) => (
-              <li key={link}>
-                <Link href={`/archive/${link}`}>{link} </Link>
-              </li>
-            ))}
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              console.log(href);
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link} </Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
