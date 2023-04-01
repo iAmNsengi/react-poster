d29466e849f33e86a9c595ee47211c13dd518279
iAmNsengi <iamnsengi@icloud.com>
2023-04-01 16:50:13 +0200

    ft: add enums and collections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+enum Gem {
+        Diamond, Sapphire, Ruby, Topaz, Onyx, Jade
+    }
+
 fn main() {
-    println!("Hello, world!");
+    // let gem1 = (Gem::Onyx, 20.00);
+    // let gem2 = (Gem::Jade, 10.00);
+    let gems = [(Gem::Onyx, 25.00), (Gem::Diamond, 100.00), (Gem::Onyx, 50.00), (Gem::Ruby, 10.00)];
+
+    for gem in gems{
+        println!("This gem is worth {}", gem.1)
+    }
+    
 }
