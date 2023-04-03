d7e791c1e5b1434a4ac706dc62f3681274ef4966
iAmNsengi <iamnsengi@icloud.com>
2023-04-03 17:24:28 +0200

    ft: get enum types through self in impl

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ enum Gem {
 
     impl fmt::Display for Gem {
         fn fmt(&self, f: &mut fmt::Formatter) ->   fmt::Result {
-            write!(f, "10")
+            match self {
+                Gem::Diamond => {write!(f,"Diamond")},
+                Gem::Sapphire => {write!(f,"Sapphire")},
+                Gem::Ruby => {write!(f,"Ruby")},
+                Gem::Topaz => {write!(f,"Topaz")},
+                Gem::Onyx => {write!(f,"Onyx")},
+                Gem::Jade => {write!(f,"Jade")},
+
+            }
         }
     }
 
@@ -16,7 +24,7 @@ fn main() {
     let gems = [(Gem::Onyx, 25.00), (Gem::Diamond, 100.00), (Gem::Onyx, 50.00), (Gem::Ruby, 10.00)];
 
     for gem in gems{
-        println!("This gem is worth {}", gem.0)
+        println!("This gem is {} and is worth {}", gem.0, gem.1)
     }
     
 }
